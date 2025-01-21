d8f591412791a63cb113165f1d415371a7f3ac4a
rayl-tanmay2 <tanmay.joshi.btech2023@sitpune.edu.in>
2025-01-21 18:50:51 +0530

    Update Calculator.java
    Add functions to perform addition, subtraction, multiplication, and division

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,27 @@
 //Calculator.java
+
+class Calculator {
+    // Addition of two numbers
+    int add(int[] numbers) {
+        return numbers[0] + numbers[1];
+    }
+    // Subtraction of two numbers
+    int subtract(int[] numbers) {
+        return numbers[0] - numbers[1];
+    }
+    // Multiplication of two numbers
+    int multiply(int[] numbers) {
+        return numbers[0] * numbers[1];
+    }
+    // Division of two numbers
+    int divide(int[] numbers) {
+        if (numbers[1] == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        return numbers[0] / numbers[1];
+    }
+}
+
+
+
+
